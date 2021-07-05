c4badd3225ae4cc136007c850181221818dc9ab4
马雄伟 <maxiongwei@vchangyi.com>
2021-07-05 23:30:48 +0800

    VueRouter中响应式原理-router、route 路由初始化解析

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,5 +18,9 @@ export default {
   components: {
     // HelloWorld,
   },
+  mounted() {
+    console.log(this.$router);
+    console.log(this.$route);
+  },
 };
 </script>
--- src/vue-router/index.js
+++ src/vue-router/index.js
@@ -1,5 +1,6 @@
 import { createWebHashHistory } from "./history/hash";
 import { createWebHistory } from "./history/html5";
+import { shallowRef, computed, reactive, unref } from "vue";
 
 // 数据处理 option.routes 是用户的配置 ，难理解不好维护
 // /  => record {Home}
@@ -54,20 +55,96 @@ function createRouterMatcher(routes) {
     matchers.push(matcher);
   }
   routes.forEach((route) => addRoute(route));
+  // 解析 {path: /, matched:HomeRecord} {path:/a,matched:{HomeRecord, aRecord}}
+  function resolve(location) {
+    const matched = []; // / /a
+    let path = location.path;
+    let matcher = matchers.find((m) => m.path == path);
+    while (matcher) {
+      matched.unshift(matcher.record); // 将用户的原始数据放到matched中
+      matcher = matchers.parent;
+    }
+    return {
+      path: path,
+      matched,
+    };
+  }
   return {
+    resolve,
     addRoute, // 动态添加路由， 面试问 路由如何动态添加 就是这个api
   };
 }
-
+const START_LOCATION_NORMALIZED = {
+  // 初始化路由系统中的默认参数
+  path: "/",
+  // params: {}, // 路由参数
+  // query: {},
+  matched: [], // 当前匹配到的记录
+};
 function createRouter(options) {
   const routerHistory = options.history;
-  const matcher = createRouterMatcher(options.routes);
+  const matcher = createRouterMatcher(options.routes); // 格式化路由的配置 拍平
 
-  console.log(matcher, routerHistory); // 格式化路由的配置 拍平
+  // TODO 这里没有理解
+  // 后续改变这个数据的value 就可以更新视图了
+  const currentRoute = shallowRef(START_LOCATION_NORMALIZED); // obj.value = reactive(响应式数据)
+
+  console.log(routerHistory, matcher);
+
+  // 将数据用计算属性 再次包裹
+
+  // 解析路径 to= '/' to={path: '/'}
+  function resolve(to) {
+    if (typeof to === "string") {
+      return matcher.resolve({ path: to });
+    }
+  }
+
+  function finalizeNavigation(to, from) {
+    if (from === START_LOCATION_NORMALIZED) {
+      routerHistory.replace(to.path);
+    } else {
+      routerHistory.push(to.path);
+    }
+    currentRoute.value = to; // 更新最新的路径
+  }
+  // 通过路径匹配到对应的记录，更新currentRoute
+  function pushWithRedirect(to) {
+    const targetLocation = resolve(to);
+    const from = currentRoute.value;
+    // 路由的钩子 再跳转前我们可以做路由的拦截
+
+    // 根据是不是第一次，来决定是push 还是replace
+    finalizeNavigation(targetLocation, from);
+    console.log(targetLocation, from);
+  }
+  function push(to) {
+    return pushWithRedirect(to);
+  }
+  //
   const router = {
+    push,
+    replace() {},
+    // 路由的核心就是 页面切换，重新渲染
     install(app) {
-      // 路由的核心就是 页面切换，重新渲染
+      let router = this;
       console.log("路由的安装", app);
+      // vue2 中有两个属性 $router 里面包含的是方法 $route 里面包含是属性
+      app.config.globalProperties.$router = router; // 方法
+      // 属性
+      Object.defineProperty(app.config.globalProperties, "$route", {
+        enumerable: true,
+        get: () => unref(currentRoute),
+      });
+      const reactiveRoute = {};
+      for (let key in START_LOCATION_NORMALIZED) {
+        reactiveRoute[key] = computed(() => currentRoute.value[key]);
+      }
+      // vuex const store = useStore();
+      app.provide("router", router); // 暴露路由对象
+      app.provide("route location", reactive(reactiveRoute)); // 用于实现 useApi
+      // let router = useRouter(); // inject('router');
+      // let route = useRoute(); // inject('route location');
       app.component("RouterLink", {
         setup:
           (_props, { slots }) =>
@@ -80,6 +157,12 @@ function createRouter(options) {
           () =>
             <div>{slots}</div>,
       });
+
+      if (currentRoute.value == START_LOCATION_NORMALIZED) {
+        // 默认初始化 ， 需要通过路由系统先进行一次跳转 发生匹配
+        push(routerHistory.location);
+      }
+
       // 后面还有逻辑
 
       // 解析路径 RouterLink RouterView 实现 页面钩子 从离开到进入 到解析完成
